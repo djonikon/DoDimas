f3e591e2d47c5c05bccd4efb51b7470910b4f1da
Dmitry <32268633+smallsoda@users.noreply.github.com>
2018-02-23 21:10:12 +0300

    Read frame
    Maybe this code doesn't work

--- hsu.h
+++ hsu.h
@@ -4,8 +4,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+    
+#define ACK_ERROR 0x10
+#define NACK      0x20
+#define ACK_OK    0x30
+    
+#define PREAMBLE_ERROR 0x10
+#define LCS_ERROR      0x20
+#define SIZE_ERROR     0x30
+#define CMD_ERROR      0x40
+#define DCS_ERROR      0x50
 
 uint8_t *create_frame(uint8_t *data, uint8_t size);
+uint8_t read_ack(UART_HandleTypeDef *huart, uint16_t timeout);
+uint8_t *read_responce(UART_HandleTypeDef *huart, uint8_t cmd, uint8_t size, uint16_t timeout);
 
 #ifdef __cplusplus
 }
